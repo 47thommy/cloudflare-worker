03181d819f75d86ef08a3726c69136f28eb54de1
47thommy <47thommy@gmail.com>
2025-04-04 23:30:27 +0300

    add a check to handle only post requests

--- openai-api-worker/src/index.js
+++ openai-api-worker/src/index.js
@@ -8,10 +8,14 @@ const corsHeaders = {
 export default {
 	async fetch(request, env, ctx) {
 
+		// handle cors preflight requests
 		if (request.method === "OPTIONS"){
 			return new Response(null, {headers:corsHeaders})
 		}
-		
+		// handle only POST requests
+		if (request.method !== "POST"){
+			return new Response(JSON.stringify({error:`${request.method} method not allowed`}), {status:405, headers:corsHeaders})
+		}
 		const openai = new OpenAI({
 			apiKey:env.OPENAI_API_KEY,
 			baseURL:"https://gateway.ai.cloudflare.com/v1/ab8c64c04d1f77f21d7ba13bc943bf38/stock_prediction/openai"
